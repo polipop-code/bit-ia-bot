04938874729a76b49eb81fdd9edee6fad8195e53
Jesus Politron <polipop.work@gmail.com>
2022-05-25 13:51:50 -0500

    cleanup 5-25-2022, project is ended here

--- bitia.js
+++ bitia.js
@@ -5,49 +5,49 @@ const config = require("./config.json");
 let prefix = config.prefix;
 
 client.on("ready", () => {
-	console.log(`Estoy lista!`);
-	client.user.setPresence({
-		status: "online",
-		game: {
-			name: "ChatLogue",
-			type: "PLAYING",
-		},
-	});
+    console.log(`Estoy lista!`);
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "ChatLogue",
+            type: "PLAYING",
+        },
+    });
 });
 
 client.on("message", (message) => {
-	if (!message.content.startsWith(prefix)) return;
-	if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-	if (command === "ping") {
-		let ping = Math.floor(message.client.ping);
-		message.channel.send({
-			embed: {
-				color: 3447003,
-				description: "pong 🏓!! " + ping + "ms",
-			},
-		});
-	} else if (command === "hello") {
-		message.channel.send({
-			embed: {
-				color: 3447003,
-				image: {
-					url: "https://i.pinimg.com/originals/95/c1/0b/95c10ba02011d6474b609c9b5adcc411.gif",
-				},
-			},
-		});
-	} else if (command === "classroom") {
-		message.guild.createChannel("📋registro", "text");
-		message.guild.createChannel("💬chat", "text");
-		message.guild.createChannel("📑tareas", "text");
-		message.guild.createChannel("📊recursos", "text");
-		message.guild.createChannel("📅tareas-atrasadas", "text");
-		message.guild.createChannel("🎓estudiantes", "voice");
-		message.guild.createChannel("🧔profesor", "voice");
-	}
+    if (command === "ping") {
+        let ping = Math.floor(message.client.ping);
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                description: "pong 🏓!! " + ping + "ms",
+            },
+        });
+    } else if (command === "hello") {
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                image: {
+                    url: "https://i.pinimg.com/originals/95/c1/0b/95c10ba02011d6474b609c9b5adcc411.gif",
+                },
+            },
+        });
+    } else if (command === "classroom") {
+        message.guild.createChannel("📋registro", "text");
+        message.guild.createChannel("💬chat", "text");
+        message.guild.createChannel("📑tareas", "text");
+        message.guild.createChannel("📊recursos", "text");
+        message.guild.createChannel("📅tareas-atrasadas", "text");
+        message.guild.createChannel("🎓estudiantes", "voice");
+        message.guild.createChannel("🧔profesor", "voice");
+    }
 });
 
 client.login(config.token);
